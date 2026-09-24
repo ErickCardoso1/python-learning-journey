ac070593d9b6893f0f2326cd59a3679052aa019c
Erick Gabriel <erickcardoso.dev@gmail.com>
2026-09-24 16:25:02 -0300

    Update player.py

--- rpg/player.py
+++ rpg/player.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import random
 
 class Player(ABC):
     def __init__(self, name, life, moves):
@@ -6,19 +7,34 @@ class Player(ABC):
         self.life = life
         self.moves = moves
     
-    
     def attack(self, target, strength):
-        ...
-    
-    def takeDamage(self):
-        ...    
-    
+        damage = random.randint(1, strength)
+        choice = random.choice(self.moves)
+        print(f'{self.name}({self.life}) attacked {target.name}({target.life}) with a {choice} of strength {strength}')
+        target.take_damage(damage)
+
+    def take_damage(self, damage):
+        self.life -= damage
+        print(f'{self.name} took {damage} damage!')
     
     @abstractmethod
     def heal(self):
         pass
 
-class Guerreiro(Player):
+class Warrior(Player):
     def __init__(self, name, life):
-        super().__init__(name, life, moves = ['Heavy Slam', 'Mercy Kill', 'Cleave'] )
+        super().__init__(name, life, moves=['Heavy Slam', 'Mercy Kill', 'Cleave'])
+    
+    def heal(self):
+        heal_power = random.randint(1, 100)
+        self.life += heal_power
+        print(f'{self.name} used healing magic, and healed {heal_power} health points')
+
+class Wizard(Player):
+    def __init__(self, name, life):
+        super().__init__(name, life, moves=['Fire Ball', 'Frostbite Burst', 'Arcane Torrent'])
         
+    def heal(self):
+        heal_power = random.randint(1, 100)
+        self.life += heal_power
+        print(f'{self.name} used a healing potion, and healed {heal_power} health points')
